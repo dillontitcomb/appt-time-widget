463ccaa078bc230c524290e23bcc29ac0a2ee7d2
Dillon <dillontitcomb@gmail.com>
2018-05-13 19:39:42 -0700

    add style element to scripts file, update widget screenshot to show injected script.

--- scripts.js
+++ scripts.js
@@ -39,6 +39,9 @@ document.addEventListener("DOMContentLoaded", function() {
        <div id='button-container'>
        </div>
      </div>
+     <style>
+       .widget-container {max-width: 600px; padding: 15px; border: 2px solid lightgrey; margin: 10px;} .row.widget {max-width: 600px; margin: 0 auto;} .time-button {background-color: #0071B2; color: white; border: 0px; border-radius: 4px 4px 4px 4px; min-width: 120px; height: 60px; margin-bottom: 10px;} .time-slot {text-align: center; width: 100%; min-height: 60px} button h4 {font-weight: 300; font-size: 16px;} .question-link {font-family: Georgia, serif; margin-right: 30px;} .heading-info {border-bottom: 4px solid #0071B2; margin-bottom: 5px; padding-bottom: 10px;} h2 {font-weight: normal;} .col-xs-3 {min-width: 122px; padding: 2px;} .widget-title {margin-bottom: 10px; font-weight: 400;} #time-button-more {background-color: white; border: 2px solid gray; color: gray;} #time-button-more h4 {font-weight: 500; }
+     </style>
      `;
      let buttonContainer = document.getElementById('button-container');
      let rowOne = document.createElement('div');
